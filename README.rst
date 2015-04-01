05989a7bc2eb9892a075073a9122306e125091a4
Hugo Rodger-Brown <hugo@yunojuno.com>
2015-04-01 20:55:59 +0100

    Initial commit
    Contains the basic project structure:
    
    setup.py
    tox file
    licence
    readme
    package folder
    tests file

---
+++ README.rst
@@ -0,0 +1,5 @@
+Client library for integrating with Base CRM API.
+
+See https://dev.futuresimple.com/
+
+Status: this is a shell project, and has no working parts as yet.
---
+++ setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""Setup file for basecrm library."""
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name="basecrm-python",
+    version="0.1",
+    packages=[
+        'basecrm',
+    ],
+    install_requires=['requests'],
+    include_package_data=True,
+    description='Base CRM API client, Python edition.',
+    long_description=README,
+    url='https://github.com/yunojuno/basecrm-python',
+    author='Hugo Rodger-Brown',
+    author_email='hugo@yunojuno.com',
+    maintainer='Hugo Rodger-Brown',
+    maintainer_email='hugo@yunojuno.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
