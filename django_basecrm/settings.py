7c516c874daf70be5ba3813e5158303ffdc37bb3
marcelkornblum <me@marcelkornblum.com>
2015-05-05 10:15:53 +0100

    URL setting defaults to latest URL

--- django_basecrm/settings.py
+++ django_basecrm/settings.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
 
-BASECRM_API_URL = getattr(settings, 'BASECRM_API_URL', None)
+BASECRM_API_URL = getattr(settings, 'BASECRM_API_URL', 'https://api.getbase.com/v2/')
 BASECRM_API_KEY = getattr(settings, 'BASECRM_API_KEY', None)
 BASECRM_USER_AGENT = getattr(settings, 'BASECRM_USER_AGENT', 'YunoJuno/1.0')
 BASECRM_INSTANTIATE_ON_START = getattr(settings, 'BASECRM_INSTANTIATE_ON_START', False)
